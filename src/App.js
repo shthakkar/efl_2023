d83947fa323dd26a6ed6b40ecba7b3ae7ba4293f
shallymysore <bkshashankhr@gmail.com>
2023-01-31 00:05:13 -0800

    Implementing routes
    added routes to all pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './style.css';
 import AllPlayers from './AllPlayers';
 import Auction from './Auction';
 import Teams from './Teams';
 import CircularNumber from './CircularNumber';
+import { Route, Routes } from 'react-router-dom'
 
 export default function App() {
-  const [page, setPage] = useState('home');
-  const handleClick = (page) => {
-    setPage(page);
+
+  const navigateToAuction = () => {
+    window.location.href = '/auction';
+  };
+
+  const navigateHome = () => {
+    window.location.href = '/home';
+  };
+  const navigateToOwners = () => {
+    window.location.href = '/owners';
+  };
+  const navigateToDailyScore = () => {
+    window.location.href = '/daily';
   };
   return (
-    <>
+    <div>
       <div className="container">
-        <button className = "mainButton" onClick={() => handleClick('home')}>Home</button>
-        <button className = "mainButton" onClick={() => handleClick('auction')}>Auction Page</button>
-        <button className = "mainButton" onClick={() => handleClick('owners')}>Owner Teams</button>
-        <button className = "mainButton" onClick={() => handleClick('daily')}>Daily Score</button>
-      </div>
-      
-      {page==="home" && <AllPlayers />}
-      {page==="auction" && <Auction />}
-      {page==="owners" && <Teams/>}
-      {page==="daily" && <CircularNumber text = {"Batsmen"} progress={5} max={15}/>}
-    </>
+          <button className="mainButton" onClick={navigateHome}>Home</button>
+          <button className="mainButton" onClick={navigateToAuction}>Auction Page</button>
+          <button className="mainButton" onClick={navigateToOwners}>Owner Teams</button>
+          <button className="mainButton" onClick={navigateToDailyScore}>Daily Score</button>
+        </div>
+        <Routes>
+          <Route path="/home" element={<AllPlayers />} />
+          <Route path="/auction" element={<Auction />} />
+          <Route path="/owners" element={<Teams />} />
+          <Route path="/daily" element={<CircularNumber />} />
+        </Routes>
+    </div>
   );
 }
