fdde77ed7fef8c8387bcf6fb75f5e79a522e8473
shallymysore <bkshashankhr@gmail.com>
2023-02-12 00:06:42 -0800

    Added Table display

--- src/OwnerStats.js
+++ src/OwnerStats.js
@@ -2,55 +2,95 @@ import React from 'react'
 import DataTable from 'react-data-table-component';
 
 export default function OwnerStats(props) {
-        const customStyles = {
-            rows: {
-                style: {
-                    maxHeight: '12px', // override the row height
-                },
-            },
-            columns:
-            {
-                style:
-                {
-                    maxWidth: '10px'
-                }
-            }
-            
-        };
-        const cols = ['Owner', 'Bat(4)','Bowl(4)','WK(1)','AR(2)','I(4-6)', 'Sq(15)'];
-        console.log(props.data)
-        function makeAbv(string) {
-            const words = string.split(' ');
-            return words.map(word => word[0].toUpperCase());
-          }
-        const countStyle = {marginRight:'10px', width: '50px', color: '#3498db',
-    };
-    const countStyleGreen = {marginRight:'10px', width: '50px', color: 'white', backgroundColor: 'lightGreen'
-    };
+  const customStyles = {
+    rows: {
+      style: {
+        maxHeight: '10px',
+        fontSize: '12px',
+        border: '1px solid black',
+        maxWidth :'600px'
+      },
+    },
+    headCells: {
+      style: {
+        border: '1px solid black',
+        color: 'blue'
+      },
+    },
+    cells: {
+      style: {
+        border: '1px solid black',
+        fontSize: '12px'
+      },
+    },
+  };
+
+  function makeAbv(string) {
+    const words = string.split(' ');
+    return words.map(word => word[0].toUpperCase());
+  }
+
+  const columns = [
+    {
+      name: 'Owner',
+      selector: row => row.ownerName,
+      width:'80px',
+      cell: row => <div style={{fontWeight: 'bold'}}>{makeAbv(row.ownerName)}</div>,
+    },
+    {
+      name: 'Bat(4)',
+      selector: row=> row.batCount,
+      width:'80px',
+      cell: row => <div style={{ backgroundColor: row.batCount >= 4 ? 'lightgreen' : 'white',textAlign:'center', padding: '4px',width: '50%',
+      height: '50%' }}>{row.batCount}</div>,
+    },
+    {
+      name: 'Bowl(4)',
+      selector: row=> row.ballCount,
+      width:'80px',
+      cell: row => <div style={{ backgroundColor: row.ballCount >= 4 ? 'lightgreen' : 'white',textAlign:'center', padding: '4px',width: '50%',
+      height: '50%' }}>{row.ballCount}</div>,
+    },
+    {
+      name: 'WK(1)',
+      selector: row=> row.wkCount,
+      width:'80px',
+      cell: row => <div style={{ backgroundColor: row.wkCount >= 1 ? 'lightgreen' : 'white',textAlign:'center', padding: '4px',width: '50%',
+      height: '50%' }}>{row.wkCount}</div>,
+    },
+    {
+      name: 'AR(2)',
+      selector: row=> row.arCount,
+      width:'80px',
+      cell: row => <div style={{ backgroundColor: row.arCount >= 2 ? 'lightgreen' : 'white',textAlign:'center', padding: '4px',width: '50%',
+      height: '50%' }}>{row.arCount}</div>,
+    },
+    {
+      name: 'I(4-6)',
+      selector: row=> row.fCount,
+      width:'80px',
+      cell: row => <div style={{ backgroundColor: row.fCount >= 4 ? 'lightgreen' : 'white',textAlign:'center', padding: '4px',width: '50%',
+      height: '50%' }}>{row.fCount}</div>,
+    },
+    {
+      name: 'Sq(15)',
+      selector: row=> row.totalCount,
+      width:'80px',
+      cell: row => <div style={{ backgroundColor: row.totalCount >= 15 ? 'lightgreen' : 'white',textAlign:'center', padding: '4px',width: '50%',
+      height: '50%' }}>{row.totalCount}</div>,
+    },
+  ];
+
   return (
-    // TITLE
-    <div style={{display:'flex',  marginRight:'10px'}}>
-            
-            <div style={{display:'flex', flexDirection:'column'}}> 
-            <div style={{display:'flex', flexDirection:'row'}}>
-            {cols.map((item, index) => (
-            <div style={{width: '50px', marginRight:'10px', color: 'blue'}} key={index}>{item}</div>
-            ))}
-            </div>
-             {props.data.map((item, index) => (
-            <div style={{display:'flex', flexDirection:'row', marginRight:'10px'}} key={index}>
-                <div style={countStyle}>{makeAbv(item.ownerName)}</div>
-                <div style={item.batCount >= 4 ? countStyleGreen : countStyle}>{item.batCount}</div>
-                <div style={item.ballCount >= 4 ? countStyleGreen : countStyle}>{item.ballCount}</div>
-                <div style={item.wkCount >= 1 ? countStyleGreen : countStyle}>{item.wkCount}</div>
-                <div style={item.arCount >= 2 ? countStyleGreen : countStyle}>{item.arCount}</div>
-                <div style={item.fCount >= 4 ? countStyleGreen : countStyle}>{item.fCount}</div>
-                <div style={item.totalCount >= 15 ? countStyleGreen : countStyle}>{item.totalCount}</div>
-                </div>
-            ))}
-            </div>
+    <div style={{width:'100%'}}>
+      <DataTable
+        title="Owners Stats"
+        columns={columns}
+        data={props.data}
+        customStyles={customStyles}
+        noHeader={true}
+        dense={true}
+      />
     </div>
-        
-    
-  )
+  );
 }
