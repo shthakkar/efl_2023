7bd2142d7bfc07ac96bc2f133ecde593e48e4032
shallymysore <bkshashankhr@gmail.com>
2023-02-11 23:58:07 -0800

    Disabling Sold and unsold button

--- src/Auction.js
+++ src/Auction.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import PlayerIntro from './PlayerIntro'
 import PlayerCard from './PlayerCard'
+import OwnerStats from './OwnerStats'
+import './style.css'; 
 
 
 export default function 
@@ -18,7 +20,6 @@ Auction() {
     useEffect(() =>{
         if (timer <= 0) {
             clearInterval(timerId.current)
-            console.log('SOLD')
         }
     }, [timer])
 
@@ -49,17 +50,34 @@ Auction() {
   const [getRandom, setData] = useState(sample);
   const [isflag, setFlag] = useState(false) 
   const [ownerToMaxBid, setOwnerToMaxBid] = useState({})
-
+  const [ownersData, setOwnersData] = useState()
+  const [isSold, setIsSold] = useState(false);
+  const [isunSold, setIsunSold] = useState(false);
+  const [buttonSold, setButtonSold] = useState(true);
+  const [buttonunSold, setButtonUnSold] = useState(true);
  async function getOwnersData()
   {
     try {
       const response = await fetch('https://efl2023test.azurewebsites.net/getallownersdata');
       if(response.ok){
         const json = await response.json();
+        setOwnersData(json)
         const data = json.reduce((acc, curr) => {
           acc[curr.ownerName] = {maxBid:curr.maxBid,currentPurse: curr.currentPurse};
           return acc;
       }, {});
+      const disableMapTemp = json.reduce((map, curr) => {
+        console.log('CURR',curr);
+        // if squad is full or foreigner count is 6 or current amount is greater than maxBid for owner
+        // set disable to true
+        if(curr.totalCount===15||curr.fCount===6||curr.maxBid<amount)
+        {
+          map[curr.ownerName]=true;
+        }
+        return map;
+    }, {});
+    console.log(disableMapTemp);
+    setDisableMap(disableMapTemp);
         console.log(data)
         setOwnerToMaxBid(data)
      } else {
@@ -76,11 +94,16 @@ Auction() {
       if(response.ok){
         const json = await response.json();
         setData(json);
-        setAmount(getRandom.salaryNumber)
+        setAmount(json.eflBase)
         setBidder('')
         setSelectedButton(null)
         setTimer(20)
         setFlag(true)
+        setFirstClick(true)
+        setIsSold(false)
+        setIsunSold(false)
+        setButtonSold(false)
+        setButtonUnSold(false)
         getOwnersData()
      } else {
        console.log('Error: ' + response.status + response.body);
@@ -90,10 +113,21 @@ Auction() {
     }
   };
   const buttonTexts = ["Gajjab Gujjus", "Bhaisaab's Royal Fixers", "My Lord Dilwale", "Dad's Army", "One Pitch One Hand", "Untouchaballs", "Lions Of Mirzapur"];
- 
+  
+
   const handleSoldClick = (inStatus,inBidder,inAmount) => {
-    const payload = { ownerTeam: inBidder , status: inStatus, boughtFor: inAmount};
+    const payload = { ownerTeam: inBidder , status: inStatus, boughtFor: inAmount, role: getRandom.role, country: getRandom.country };
     console.log(inStatus,inBidder,inAmount)
+    if (inStatus === 'sold')
+    {
+      setIsSold(true)
+      setButtonSold(true)
+    }
+    else
+    {
+      setIsunSold(true)
+      setButtonUnSold(true)
+    }
     fetch('https://efl2023test.azurewebsites.net/updateplayer/'+getRandom._id.$oid, {
       method: 'PUT',
       headers: {
@@ -110,8 +144,16 @@ Auction() {
     });
     setFlag(false)
   }
+  const [firstClick,setFirstClick] = useState(true)
+
   function increaseAmount()
   {
+    // for the first bid set to base price
+    if(firstClick)
+    {
+      setFirstClick(false)
+      return
+    }
     let increment = 5;
     if (amount >= 200)
     {
@@ -120,16 +162,44 @@ Auction() {
       increment = 10;
     }
     setAmount(amount+increment)
+    const disableMapTemp = ownersData.reduce((map, curr) => {
+      console.log('CURR',curr);
+      // if squad is full or foreigner count is 6 or current amount is greater than maxBid for owner
+      // set disable to true
+      if(curr.totalCount===15||curr.fCount===6||curr.maxBid<amount)
+      {
+        map[curr.ownerName]=true;
+      }
+      return map;
+  }, {});
+  console.log(disableMapTemp);
+  setDisableMap(disableMapTemp);
   }
+/*
+  function shouldBeEnabled(teamName){
+    const currentRole = getRandom.role;
+    const currentCountry = getRandom.country;
+    const ownerForeignerCount = getOwnerAttr(teamName);
+    const ownerRoleCount = getOwnerAttr(teamName, currentRole);
+
+    if(currentCountry.toLowerCase()!="india" && ownerForeignerCount>=6){
+      return false;
+    }
 
 
+    
+  }*/
+const [disableMap, setDisableMap] = useState({})
       
      // const getRandom = null
   return (
     <div className="App">
       <div className="top-row">
         <div className="top-row-item">
+        <div style={{display: "flex", flexDirection:"column"}}>
         <PlayerIntro intro={getRandom.intro}/>
+        {ownersData && <OwnerStats data={ownersData}/> }
+        </div>
         </div>
         <div className="top-row-item">
            { console.log(getRandom.type)}
@@ -150,23 +220,28 @@ Auction() {
       </div>
       <div>
         {isflag &&(
-          <div>
-           <h1>Time Remaining: {timer}</h1>
-           </div>) }
+          <div className="time-text show">Time Remaining: {timer}</div>) }
       </div>
+      <div>
+        {isSold && <div className="sold-text show">SOLD</div>}
+      </div>
+      <div>
+        {isunSold && <div className="unsold-text show">UNSOLD</div>}
+      </div>
+
       <div className="bottom-row">
       
       {buttonTexts.map((text, index) => (
         <div key={index} className="container-for-team">
           <img src={require('./auction_hand.png')} alt="my-image" className="my-image" style={{ display: selectedButton === index ? 'block' : 'none' }}/>
-          <button id= {text}  onClick={() => {setSelectedButton(index)
-          setBidder(text); increaseAmount();setTimer(20)}} className="my-button teamButton">{text}</button>
+          <button id= {text} disabled={disableMap[text]} onClick={() => {setSelectedButton(index)
+          setBidder(text); increaseAmount();setTimer(20)}} className={`${disableMap[text] ? "button-disabled" : "my-button teamButton"}`}>{text}</button>
       
         </div>
       ))}
       <button className="action-button" onClick={handleClick}>Next Player</button>
-      <button className="action-button" onClick={()=>handleSoldClick('sold', bidder, amount)}>Mark Sold</button>
-      <button className="action-button" onClick={()=>handleSoldClick('unsold-processed','',0)}>Mark Unsold</button>
+      <button className="action-button" onClick={()=>handleSoldClick('sold', bidder, amount)} disabled={buttonSold}>Mark Sold</button>
+      <button className="action-button" onClick={()=>handleSoldClick('unsold-processed','',0)} disabled={buttonunSold}>Mark Unsold</button>
       
       
       </div>
