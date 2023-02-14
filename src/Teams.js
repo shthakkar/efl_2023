3db54fbf4daa2b329c3a1262c9bd8c613b8588e3
shallymysore <bkshashankhr@gmail.com>
2023-02-14 00:58:09 -0800

    Update Teams.js

--- src/Teams.js
+++ src/Teams.js
@@ -8,10 +8,36 @@ import TeamRestrictions from "./TeamRestrictions";
 export default function Teams() {
     const [Playerslist, setPlayerslist] = useState([]);
     
+    const customTableStyles = {
+    rows: {
+      style: {
+        maxHeight: '10px',
+        fontSize: '12px',
+        border: '1px solid black',
+        backgroundColor: 'green',
+      },
+    },
+    headCells: {
+      style: {
+        border: '2px solid black',
+        fontSize: '16px',
+        fontWeight: 'Bold',
+        color: 'blue'
+      },
+    },
+    cells: {
+      style: {
+        border: '1px solid black',
+        color: 'black',
+        fontSize: '14px'
+      },
+    },
+  };
+    
     useEffect(() => {
         async function getallplayers(){
             try {
-                const response = await fetch('https://efl2023test.azurewebsites.net/getallplayers');
+                const response = await fetch('https://testefl2023.azurewebsites.net/getallplayers');
                 if(response.ok){
                     const data = await response.json();
                     //console.log(data)
@@ -48,11 +74,12 @@ export default function Teams() {
   return (
     <div>
       {Object.keys(groupedData).map(groupKey => (
-        <div key={groupKey}>
+        <div style={{fontSize: '20px',fontWeight: 'Bold',color: 'black',textAlign:'center'}} key={groupKey}>
           <h2>{groupKey}</h2>
           <DataTable
             data={groupedData[groupKey]}
             columns={columns}
+            customTableStyles = {customTableStyles}
             sortable
             defaultSortFieldId="boughtFor"
           />
