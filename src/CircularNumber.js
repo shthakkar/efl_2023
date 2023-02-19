d83c911ae43795b008e1459b141890ec44cff5d3
shallymysore <bkshashankhr@gmail.com>
2023-02-18 20:33:28 -0800

    coloring and styling

--- src/CircularNumber.js
+++ src/CircularNumber.js
@@ -6,13 +6,15 @@ const CircularNumber = ({ progress, max , text}) => {
   const progressPercentage = (progress / max) * 100;
   
   return (
-    <div style={{width: "50px", height : "50px", marginRight:"30px", marginBottom: "20px", marginTop: "20px"}}>
+    <div style={{width: "50px", height : "50px", marginRight:"40px", marginBottom: "20px", marginTop: "20px"}}>
       <CircularProgressbar
         value={progressPercentage}
         strokeWidth={50}
         
         styles={buildStyles({
-          strokeLinecap: "butt"
+          strokeLinecap: "butt",
+          pathColor:"brown",
+          trailColor:"beige"
         })}
       />
       <div style={{fontSize:"15px"}}>{text}  {progress}/{max}</div>
