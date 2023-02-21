faedde0af3dfa97b3f38a202d32e27e15f3d9ac3
shallymysore <bkshashankhr@gmail.com>
2023-02-20 22:52:46 -0800

    changed player intro

--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
- const PlayerCard = ({playerName, country, type, franchise}) => {
+ const PlayerCard = ({playerName, country,intro1,intro2,intro3, type, franchise}) => {
   return (
     <div className="player-card">
-      <h1 className="player-name" style={{fontSize: '32px'}}>{playerName}</h1>
-      <h2 className="player-country" style={{fontSize: '24px'}}>{country}</h2>
-      <h3 className="player-type" style={{fontSize: '20px'}}>{type}</h3>
-      <h3 className="player-franchise" style={{fontSize: '20px'}}>{franchise}</h3>
+      <h1 className="player-name" style={{fontSize: '32px'}}>{playerName} ({franchise})</h1>
+      <h2 className="player-country" style={{fontSize: '24px'}}>{type} from {country}</h2>
+      <h3 className="player-intro" style={{fontSize: '14px',margin:"unset"}}>{intro1}</h3>
+      <h4 className="player-intro" style={{fontSize: '14px',margin:"unset"}}>{intro2}</h4>
+      <h5 className="player-intro" style={{fontSize: '14px',margin:"unset"}}>{intro3}</h5>
     </div>
   );
 }
 
 export default PlayerCard;
+
