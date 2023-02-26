1245b237366b0649db58cfec73c1d666fc86b524
shallymysore <bkshashankhr@gmail.com>
2023-02-25 22:45:24 -0800

    changing font

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export default function ProgressBar(props) {
-    const { label,progress, max } = props;
+  const { label,progress, max } = props;
   const width = (progress / max) * 100;
   const isFull = progress >= max
   return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
+    <div style={{ display: 'flex', alignItems: 'center',fontSize:'15px' }}>
         {{label}&&<div>{label}</div>}
         <div>{progress}/{max}</div>
         <div style={{ width: `${width}%`, background: isFull? 'green' : 'yellow', height: '20px' }} />
