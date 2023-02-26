bb7f8b029a73894b9f8319b9acbdbca9549dc360
shallymysore <bkshashankhr@gmail.com>
2023-02-25 22:40:32 -0800

    changed margin to fit new teams page

--- src/CircularNumber.js
+++ src/CircularNumber.js
@@ -6,15 +6,15 @@ const CircularNumber = ({ progress, max , text}) => {
   const progressPercentage = (progress / max) * 100;
   
   return (
-    <div style={{width: "50px", height : "50px", marginRight:"40px", marginBottom: "20px", marginTop: "20px"}}>
+    <div style={{width: "50px", height : "50px", marginRight:"40px", marginBottom: "40px", marginTop: "20px"}}>
       <CircularProgressbar
         value={progressPercentage}
         strokeWidth={50}
         
         styles={buildStyles({
           strokeLinecap: "butt",
-          pathColor:"brown",
-          trailColor:"beige"
+          pathColor:"red",
+          trailColor:"yellow"
         })}
       />
       <div style={{fontSize:"15px"}}>{text}  {progress}/{max}</div>
