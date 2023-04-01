d60ce7e26ce3a98215c3eca9e01d81e5cdada755
shallymysore <bkshashankhr@gmail.com>
2023-03-31 22:51:07 -0700

    dummy code

---
+++ src/SubstitutePlayers.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useMemo} from "react";
+import './style.css';
+import DataTable from 'react-data-table-component';
+import { createTheme } from "react-data-table-component";
+
+
+export default function SubstitutePlayers() {
+
+        return("Page is under development")
+}
